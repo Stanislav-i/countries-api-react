362c3e7a71cfa3dd544b79bee28eb59c5c0ead93
Stanislav-i <itandeducationua@gmail.com>
2023-09-13 19:25:41 +0300

    header added/theme mode added

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Container } from './Container';
+import { IoMoonOutline, IoMoon } from 'react-icons/io5';
+
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
+
+const Title = styled.a.attrs({
+  href: '/',
+})`
+  color: var(--color-text);
+  font-size: var(--fv-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+
+const ModeSwitcher = styled.div`
+  color: var(--color-text);
+  font-size: var(--fv-sm);
+  font-weight: var(--fw-bold);
+  cursor: pointer;
+  text-transform: capitalize;
+`;
+
+export const Header = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <HeaderEl>
+      <Container>
+        <Wrapper>
+          <Title>Your countries</Title>
+          <ModeSwitcher onClick={toggleTheme}>
+            {theme === 'light' ? (
+              <IoMoonOutline size="14px" />
+            ) : (
+              <IoMoon size="14px" />
+            )}
+            <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
+          </ModeSwitcher>
+        </Wrapper>
+      </Container>
+    </HeaderEl>
+  );
+};
